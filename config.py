069bb6552fc3cf4d4a11965bc5f9a9b4ac5ccc70
7s.i <7s.i@pm.me>
2025-12-26 02:24:21 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SYMBOL = os.getenv("SYMBOL", "QRL/USDT")
+
+TIMEFRAME = "1d"          # QRL 用日線最穩
+BASE_ORDER_USDT = 50      # 單筆最多用多少 USDT
+MAX_POSITION_USDT = 500   # QRL 最大曝險
+PRICE_OFFSET = 0.98       # 限價折讓（不追價）
